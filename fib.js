318a514f3092e5af668f36fbd34d0d6ef86ae0e6
Derek Louie <derek.k.louie@gmail.com>
2014-06-20 03:58:54 -0700

    Cleaned up js code

--- fib.js
+++ fib.js
@@ -1,53 +1,57 @@
 !function(e,t,n){function o(e,n){var r=t.createElement(e||"div"),i;for(i in n)r[i]=n[i];return r}function u(e){for(var t=1,n=arguments.length;t<n;t++)e.appendChild(arguments[t]);return e}function f(e,t,n,r){var o=["opacity",t,~~(e*100),n,r].join("-"),u=.01+n/r*100,f=Math.max(1-(1-e)/t*(100-u),e),l=s.substring(0,s.indexOf("Animation")).toLowerCase(),c=l&&"-"+l+"-"||"";return i[o]||(a.insertRule("@"+c+"keyframes "+o+"{"+"0%{opacity:"+f+"}"+u+"%{opacity:"+e+"}"+(u+.01)+"%{opacity:1}"+(u+t)%100+"%{opacity:"+e+"}"+"100%{opacity:"+f+"}"+"}",a.cssRules.length),i[o]=1),o}function l(e,t){var i=e.style,s,o;if(i[t]!==n)return t;t=t.charAt(0).toUpperCase()+t.slice(1);for(o=0;o<r.length;o++){s=r[o]+t;if(i[s]!==n)return s}}function c(e,t){for(var n in t)e.style[l(e,n)||n]=t[n];return e}function h(e){for(var t=1;t<arguments.length;t++){var r=arguments[t];for(var i in r)e[i]===n&&(e[i]=r[i])}return e}function p(e){var t={x:e.offsetLeft,y:e.offsetTop};while(e=e.offsetParent)t.x+=e.offsetLeft,t.y+=e.offsetTop;return t}var r=["webkit","Moz","ms","O"],i={},s,a=function(){var e=o("style",{type:"text/css"});return u(t.getElementsByTagName("head")[0],e),e.sheet||e.styleSheet}(),d={lines:12,length:7,width:5,radius:10,rotate:0,corners:1,color:"#000",speed:1,trail:100,opacity:.25,fps:20,zIndex:2e9,className:"spinner",top:"auto",left:"auto",position:"relative"},v=function m(e){if(!this.spin)return new m(e);this.opts=h(e||{},m.defaults,d)};v.defaults={},h(v.prototype,{spin:function(e){this.stop();var t=this,n=t.opts,r=t.el=c(o(0,{className:n.className}),{position:n.position,width:0,zIndex:n.zIndex}),i=n.radius+n.length+n.width,u,a;e&&(e.insertBefore(r,e.firstChild||null),a=p(e),u=p(r),c(r,{left:(n.left=="auto"?a.x-u.x+(e.offsetWidth>>1):parseInt(n.left,10)+i)+"px",top:(n.top=="auto"?a.y-u.y+(e.offsetHeight>>1):parseInt(n.top,10)+i)+"px"})),r.setAttribute("aria-role","progressbar"),t.lines(r,t.opts);if(!s){var f=0,l=n.fps,h=l/n.speed,d=(1-n.opacity)/(h*n.trail/100),v=h/n.lines;(function m(){f++;for(var e=n.lines;e;e--){var i=Math.max(1-(f+e*v)%h*d,n.opacity);t.opacity(r,n.lines-e,i,n)}t.timeout=t.el&&setTimeout(m,~~(1e3/l))})()}return t},stop:function(){var e=this.el;return e&&(clearTimeout(this.timeout),e.parentNode&&e.parentNode.removeChild(e),this.el=n),this},lines:function(e,t){function i(e,r){return c(o(),{position:"absolute",width:t.length+t.width+"px",height:t.width+"px",background:e,boxShadow:r,transformOrigin:"left",transform:"rotate("+~~(360/t.lines*n+t.rotate)+"deg) translate("+t.radius+"px"+",0)",borderRadius:(t.corners*t.width>>1)+"px"})}var n=0,r;for(;n<t.lines;n++)r=c(o(),{position:"absolute",top:1+~(t.width/2)+"px",transform:t.hwaccel?"translate3d(0,0,0)":"",opacity:t.opacity,animation:s&&f(t.opacity,t.trail,n,t.lines)+" "+1/t.speed+"s linear infinite"}),t.shadow&&u(r,c(i("#000","0 0 4px #000"),{top:"2px"})),u(e,u(r,i(t.color,"0 0 1px rgba(0,0,0,.1)")));return e},opacity:function(e,t,n){t<e.childNodes.length&&(e.childNodes[t].style.opacity=n)}}),function(){function e(e,t){return o("<"+e+' xmlns="urn:schemas-microsoft.com:vml" class="spin-vml">',t)}var t=c(o("group"),{behavior:"url(#default#VML)"});!l(t,"transform")&&t.adj?(a.addRule(".spin-vml","behavior:url(#default#VML)"),v.prototype.lines=function(t,n){function s(){return c(e("group",{coordsize:i+" "+i,coordorigin:-r+" "+ -r}),{width:i,height:i})}function l(t,i,o){u(a,u(c(s(),{rotation:360/n.lines*t+"deg",left:~~i}),u(c(e("roundrect",{arcsize:n.corners}),{width:r,height:n.width,left:n.radius,top:-n.width>>1,filter:o}),e("fill",{color:n.color,opacity:n.opacity}),e("stroke",{opacity:0}))))}var r=n.length+n.width,i=2*r,o=-(n.width+n.length)*2+"px",a=c(s(),{position:"absolute",top:o,left:o}),f;if(n.shadow)for(f=1;f<=n.lines;f++)l(f,-2,"progid:DXImageTransform.Microsoft.Blur(pixelradius=2,makeshadow=1,shadowopacity=.3)");for(f=1;f<=n.lines;f++)l(f);return u(t,a)},v.prototype.opacity=function(e,t,n,r){var i=e.firstChild;r=r.shadow&&r.lines||0,i&&t+r<i.childNodes.length&&(i=i.childNodes[t+r],i=i&&i.firstChild,i=i&&i.firstChild,i&&(i.opacity=n))}):s=l(t,"animation")}(),typeof define=="function"&&define.amd?define(function(){return v}):e.Spinner=v}(window,document);
 
 var opts = { lines: 13, length: 7, width: 4, radius: 10, corners: 1, rotate: 0, color: '#000', speed: 1, trail: 60, shadow: false, hwaccel: false, className: 'spinner', zIndex: 2e9, top: 'auto', left: 'auto' },
-thecache = {"0":0,"1":1};
+thecache = {};
 
-function timingFunction(n) {
-    var target = document.getElementById('centered');
-    var spinner = new Spinner(opts).spin(target);
-
-    setTimeout(function() {
-            var start2 = new Date();
-            fibwithcache(n);
-            var stop2 = new Date();
-            var executionTime2 = stop2 - start2;
-
-            setTimeout(function() {
-                var start1 = new Date();
-                fibslow(n);
-                var stop1 = new Date();
-                var executionTime1 = stop1 - start1;
-
-                setTimeout(function() {
-
-                    $(".cache").html("Your Cache: "+JSON.stringify(thecache));
-                    $(".answer").html("Answer: "+thecache[n]);
-                    $("#wcache").html(executionTime2 );
-                    $("#wocache").html(executionTime1 );
-                    if(n > 29) {$(".note").html("* NOTE: As you will soon notice, the order of magnitude a cache improves this by is exponential. I think something like 40 billion calculations <br/> used to calculate the 50th fib number without a cache is brought down to 99 calculations with one, so it is quite an increase in performance!")}
-                    spinner.stop();
-                    }, 0); }, 0); }, 0);
-
+function timeFunction(args,cb) {
+    var start = new Date(),
+    dfd = new jQuery.Deferred();
+    cb.call(this,args);
+    var theTimeout = setTimeout(function() {dfd.resolve(new Date() - start);}, 0);
+    return dfd.promise();
 }
 
+function renderFibResults() {
+    var spinner = new Spinner(opts).spin(target);
+    var target = document.getElementById('centered');
+    var n = $('#fibnum')[0].value;
+
+    $(".cache").html("Your Cache: "+JSON.stringify(thecache));
+
+    timeFunction(n,fibwithcache).then(function(val) {
+        $("#wcache").html(val);
+    });
+
+    timeFunction(n,fibslow).then(function(val) {
+        $("#wocache").html(val);
+    });
+
+
+    $(".answer").html("Answer: "+ thecache[n]);
+
+    if(n > 35) {$(".note").html("* NOTE: As you will soon notice, the order of magnitude a cache improves this by is exponential. I think something like 40 billion calculations <br/> used to calculate the 50th fib number without a cache is brought down to 99 calculations with one, so it is quite an increase in performance!")}
+
+    $(".cache").html("Your Cache: "+JSON.stringify(thecache));
+    spinner.stop();
+}
 
 function fibslow(n) {
-    if (n == 0) {return n;}
-    else if (n == 1 ) {return n; }
-    else {return fibslow(n-1)+fibslow(n-2)}
-}   
-function fibwithcache(n)
-{
     if (n <= 1) {
-        return n;}
-    else if(thecache[n] == undefined) 
-    { thecache[n] = fibwithcache(n-1)+fibwithcache(n-2);
-        return thecache[n];
-    } 
-    else 
-    {   
-        return thecache[n];  
+        return n;
+    } else {
+        return fibslow(n-1) + fibslow(n-2);
     }
-
 }
+
+function fibwithcache(n) {
+    if (!thecache[n]) {
+        if (n <= 1) {
+            thecache[n] = n;
+        } else {
+            thecache[n] = fibwithcache(n-1) + fibwithcache(n-2);
+        }
+    }
+    return thecache[n];
+}
+
+$(".btn").on('click', renderFibResults);
